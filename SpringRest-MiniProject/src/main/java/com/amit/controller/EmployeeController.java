bef6ce053a8eb5f0b0417f5d730b992f47b575fb
amityadav809001 <amityadav80900@gmail.com>
2021-07-15 10:02:30 +0530

    Unit test cases update

--- SpringRest-MiniProject/src/main/java/com/amit/controller/EmployeeController.java
+++ SpringRest-MiniProject/src/main/java/com/amit/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -32,6 +33,7 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/api/employee")
 @Api(description = "Employee Operation")
 @Profile(value = { "default", "qa" })
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
 	@Autowired
--- SpringRest-MiniProject/src/test/java/com/amit/SpringRestMiniProjectApplicationTests.java
+++ SpringRest-MiniProject/src/test/java/com/amit/SpringRestMiniProjectApplicationTests.java
@@ -1,13 +1,106 @@
 package com.amit;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
-@SpringBootTest
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.properties")
 class SpringRestMiniProjectApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("EMPLOYEE SAVE TEST")
+	@Order(1)
+	public void testCreateEmployee() throws Exception {
+
+		// 1.Creating HttpRequest Using Mock..
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/api/employee/save")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"empName\" : \"amityadav\", \"empSal\": 2000.0, \"empDept\" : \"DEV\" }");
+
+		// 2.Excecute request and Read Result
+		MvcResult result = mockMvc.perform(request).andReturn();
+
+		// 3.Read Response From Result
+		MockHttpServletResponse response = result.getResponse();
+		
+		//4.validate Response
+		    assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+		    if(!response.getContentAsString().contains("Created!"))
+		    {
+		    	fail("Save is not working properly.");
+		    }
+	}
+	
+
+	@Test
+	@DisplayName("EMPLOYEE ALL TEST")
+	@Order(2)
+	public void testGetAllEmployees() throws Exception {
+
+		// 1.Creating HttpRequest Using Mock..
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/api/employee/all");
+				
+
+		// 2.Excecute request and Read Result
+		MvcResult result = mockMvc.perform(request).andReturn();
+
+		// 3.Read Response From Result
+		MockHttpServletResponse response = result.getResponse();
+		
+		//4.validate Response
+		    assertEquals(HttpStatus.OK.value(), response.getStatus());
+		    assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType());
+		    
+	}
+	@Test
+	@DisplayName("DELETE ONE EMPLOYEE")
+	@Order(3)
+	public void testDeleteOneEmployee() throws Exception {
+
+		// 1.Creating HttpRequest Using Mock..
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/api/employee/remove/{id}",9);
+				
+
+		// 2.Excecute request and Read Result
+		MvcResult result = mockMvc.perform(request).andReturn();
+
+		// 3.Read Response From Result
+		MockHttpServletResponse response = result.getResponse();
+		
+		//4.validate Response
+		    assertEquals(HttpStatus.OK.value(), response.getStatus());
+		    if(!response.getContentAsString().contains("Deleted"))
+		    {
+		    	fail("Record id not deleted!.");
+		    }
+		   
+		    
 	}
 
 }
