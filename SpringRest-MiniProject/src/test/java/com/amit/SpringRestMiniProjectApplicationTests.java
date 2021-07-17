5b9d022f99c788e2bb6067f33181921be656146f
amityadav809001 <amityadav80900@gmail.com>
2021-07-17 18:01:38 +0530

    unit test cases.

--- SpringRest-MiniProject/src/test/java/com/amit/SpringRestMiniProjectApplicationTests.java
+++ SpringRest-MiniProject/src/test/java/com/amit/SpringRestMiniProjectApplicationTests.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -46,15 +47,13 @@ class SpringRestMiniProjectApplicationTests {
 
 		// 3.Read Response From Result
 		MockHttpServletResponse response = result.getResponse();
-		
-		//4.validate Response
-		    assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-		    if(!response.getContentAsString().contains("Created!"))
-		    {
-		    	fail("Save is not working properly.");
-		    }
+
+		// 4.validate Response
+		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+		if (!response.getContentAsString().contains("Created!")) {
+			fail("Save is not working properly.");
+		}
 	}
-	
 
 	@Test
 	@DisplayName("EMPLOYEE ALL TEST")
@@ -64,43 +63,118 @@ class SpringRestMiniProjectApplicationTests {
 		// 1.Creating HttpRequest Using Mock..
 
 		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/api/employee/all");
-				
 
 		// 2.Excecute request and Read Result
 		MvcResult result = mockMvc.perform(request).andReturn();
 
 		// 3.Read Response From Result
 		MockHttpServletResponse response = result.getResponse();
-		
-		//4.validate Response
-		    assertEquals(HttpStatus.OK.value(), response.getStatus());
-		    assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType());
-		    
+
+		// 4.validate Response
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+		assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType());
+
 	}
+
 	@Test
 	@DisplayName("DELETE ONE EMPLOYEE")
-	@Order(3)
+	@Order(6)
+	@Disabled
 	public void testDeleteOneEmployee() throws Exception {
 
 		// 1.Creating HttpRequest Using Mock..
 
-		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/api/employee/remove/{id}",9);
-				
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/api/employee/remove/{id}", 22);
 
 		// 2.Excecute request and Read Result
 		MvcResult result = mockMvc.perform(request).andReturn();
 
 		// 3.Read Response From Result
 		MockHttpServletResponse response = result.getResponse();
-		
-		//4.validate Response
-		    assertEquals(HttpStatus.OK.value(), response.getStatus());
-		    if(!response.getContentAsString().contains("Deleted"))
-		    {
-		    	fail("Record id not deleted!.");
-		    }
-		   
-		    
+
+		// 4.validate Response
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+		if (!response.getContentAsString().contains("Deleted")) {
+			fail("Record id not deleted!.");
+		}
+
+	}
+
+	@Test
+	@DisplayName("EMPLOYEE DELETE NOT FOUND TEST")
+	@Order(5)
+	public void testEmployeeDeleteNotFound() throws Exception {
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete("/api/employee/remove/{id}", 100);
+
+		MvcResult result = mockMvc.perform(request).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), response.getStatus());
+
+	}
+
+	@Test
+	@DisplayName("Test EMPLOYEE GET ONE")
+	@Order(3)
+	public void testEmployeesGetOne() throws Exception {
+
+		// 1.Creating HttpRequest Using Mock..
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/api/employee/one/{id}", 20);
+
+		// 2.Excecute request and Read Result
+		MvcResult result = mockMvc.perform(request).andReturn();
+
+		// 3.Read Response From Result
+		MockHttpServletResponse response = result.getResponse();
+
+		// 4.validate Response
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+	}
+
+	@Test
+	@DisplayName("Test EMPLOYEE GET ONE NOT FOUND")
+	@Order(4)
+	public void testEmployeesGetOneNotFound() throws Exception {
+
+		// 1.Creating HttpRequest Using Mock..
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/api/employee/one/{id}", 200);
+
+		// 2.Excecute request and Read Result
+		MvcResult result = mockMvc.perform(request).andReturn();
+
+		// 3.Read Response From Result
+		MockHttpServletResponse response = result.getResponse();
+
+		// 4.validate Response
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), response.getStatus());
+
+	}
+	
+	@Test
+	@DisplayName("EMPLOYEE UPDATE TEST")
+	@Order(7)
+	public void testEmployeeUpdate() throws Exception {
+
+		// 1.Creating HttpRequest Using Mock..
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/api/employee/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"empId\":23,\"empName\" : \"amityadavUpdate\", \"empSal\": 1000.0, \"empDept\" : \"Devupdate\" }");
+
+		// 2.Excecute request and Read Result
+		MvcResult result = mockMvc.perform(request).andReturn();
+
+		// 3.Read Response From Result
+		MockHttpServletResponse response = result.getResponse();
+
+		// 4.validate Response
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+		if (!response.getContentAsString().contains("Updated")) {
+			fail("Update is not working properly.");
+		}
 	}
 
 }
